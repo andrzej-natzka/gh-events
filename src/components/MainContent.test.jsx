281be97eb8aeb0d663e9dc268ff76d99fff1870e
Andrzej Kozlowski <andrzej.kozlowski@natzka.com>
2023-03-21 23:00:02 +0100

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 // Comment
+// Another comment
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
